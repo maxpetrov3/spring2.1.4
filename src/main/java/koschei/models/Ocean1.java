ca19a86832056c9aa45f885b9434673114804d6c
maxpetrov3 <max.com.07@mail.ru>
2021-10-16 17:59:47 +0400

    Exercise spring 2.1.4 done;

--- src/main/java/koschei/models/Ocean1.java
+++ src/main/java/koschei/models/Ocean1.java
@@ -1,12 +1,19 @@
 package koschei.models;
 
+import koschei.config.AppConfig;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.stereotype.Component;
+
+import javax.security.auth.login.Configuration;
 
 @Component
 public class Ocean1 {
 
     @Autowired
+    @Qualifier("island")
     private Island2 island;
 
     @Override
